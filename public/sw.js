407bf24a3b1135ef89d39fbc3540039d4ad2fce7
Duk.Kyeon <nguyenkien.8369@gmail.com>
2026-08-02 02:56:12 +0700

    Refresh cache for cropped LiftPath icon

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "liftpath-shell-v7";
+const CACHE_NAME = "liftpath-shell-v8";
 const scopeUrl = new URL("./", self.registration.scope).href;
-const appIconUrl = new URL("app-icon-v3.svg?v=3", self.registration.scope).href;
+const appIconUrl = new URL("app-icon.svg?v=4", self.registration.scope).href;
 const shellAssets = [scopeUrl, new URL("manifest.webmanifest", self.registration.scope).href, appIconUrl];
 let restTimeout = null;
 
